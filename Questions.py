97aab9397bc0e270a36673f607902b8cefc1c2af
Saul Oremus <sauloremus@Sauls-MBP.lan>
2023-12-07 12:01:59 -0500

    first commit

---
+++ Questions.py
@@ -0,0 +1,45 @@
+# Question 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+
+# def hello_name(user_name):
+#        print(f"hello_{user_name.upper()}")  
+# print(hello_name ("Saul"))
+
+# Question 2
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+# def first_odds():
+#     for num in range (1, 101, 2):
+#         print(num)
+# print(first_odds())   
+               
+# Question 3
+# Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+
+# def max_num_in_list(a_list):
+#     return max(a_list) 
+# print(max_num_in_list([2, 4, 6, 7, 8, 9]))
+
+               
+# Question 4
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+# def is_leap_year(2014):
+#     if is_leap_year is %4 == 0 and %400 == 0
+#        print("is leap year")
+#     elif 
+#         print("not a leap year")
+#         .....
+               
+# Question 5
+# Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+# def is_consecutive ([1,2,3,4,5])
+#     if is_consecutive [x] - is_consecutive [x-1] == 1:
+#         print("The list is consecutive.")
+#     else:
+#         print("The list is not consecutive.")
+
+
+
+
